4fc6998fcb1e31818856c9d6da71e527b9c1d6a7
webrcost4 <webrcost4@gmail.com>
2024-04-19 10:10:18 -0300

    a

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import * as express from 'express';
 import { NestFactory } from '@nestjs/core';
 import { MoviesModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MoviesModule);
+  app.use(express.json());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
